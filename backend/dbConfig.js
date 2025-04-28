63c9c664a3d3ca5db61bbf6f23e9646c80e79437
AT <ashutoshkumar63041@gmail.com>
2025-04-28 21:36:39 +0530

    Update dbConfig.js

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -7,7 +7,11 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false },
+    idleTimeoutMillis: 30000, 
+    connectionTimeoutMillis: 5000
+
+    
 });
 
 module.exports = pool;
